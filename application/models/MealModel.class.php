69820319ef49e5ad1a2b739c47fba56e2734ee76
Geoffrey01 <longa.geoffrey@gmail.com>
2018-10-09 14:36:00 +0200

    debut class mealmodel

--- application/models/MealModel.class.php
+++ application/models/MealModel.class.php
@@ -1,1 +1,22 @@
 <?php
+
+class MealModel
+{
+  private $sql;
+  private $database;
+
+  public function __construct()
+  {
+    $this->sql = $sql;
+    $this->database = new Database
+    (
+
+    );
+  }
+}
+
+
+
+
+
+// $sql = 'SELECT Name, Photo, Description, SalePrice FROM meal';
