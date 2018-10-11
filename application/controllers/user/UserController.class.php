7b292f70146327150d9281858f8919164b600607
Geoffrey01 <longa.geoffrey@gmail.com>
2018-10-11 14:45:39 +0100

    no message

--- application/controllers/user/UserController.class.php
+++ application/controllers/user/UserController.class.php
@@ -1,2 +1,17 @@
 <?php
 //gestion de la user session + inscription + messages d'erreurs
+
+class UserControler
+{
+  public function httpGetMethod(Http $http, array $queryFields)
+  {
+    
+
+  }
+
+  public function httpPostMethod(Http $http, array $formFields)
+  {
+
+
+  }
+}
