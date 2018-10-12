1a41f30986270adfe54a2debec0fe01e91fb76ab
mateo angel lopez <angellopezmateo82@gmail.com>
2018-10-12 12:18:13 +0200

    usersession

--- application/classes/UserSession.class.php
+++ application/classes/UserSession.class.php
@@ -2,17 +2,89 @@
 
 class UserSession
 {
-  public function __construct()
-  {
-    if ()
+    public function __construct()
     {
-      // démarrage du module
+        if (session_status() === PHP_SESSION_NONE)
+        {
+        // démarrage du module
+        session_start();    
+        }
     }
-  }
 
-  public function create( , , , )
-  {
+    public function create($email,$firstName,$lastName,$userId)
+    {
+        $_SESSION["user"]= array
+        (
+            "email" => $email,
+            "firstName" => $firstName,
+            "lastName" => $lastName,
+            "userId" => $userId  
+        );
+    }
     
-  }
+    public function destroy()
+    {
+        session_unset($_SESSION["user"]);
+        session_destroy();
+    }
+    
+    public function isAuthenticated()
+    {
+    //verif si la clés du user existe on retourn ou vrai ou faux
+        if (array_key_exist("user", $_SESSION) == true)
+        {
+            if(empty($_SESSION["user"]) == false)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public function getEmail()
+    {
+        
+        if (IsAuthenticated() == true) 
+        {
+            return $_SESSION['user']['email'];
+        }
+        return null;
+    }
+    
+    public function getLastName()
+    {
+        if (IsAuthenticated() == true) 
+        {
+            return $_SESSION['user']['lastName'];
+        }
+        return null;
+    }
+    
+    public function getFirstName()
+    {
+        if (IsAuthenticated() == true) 
+        {
+            return $_SESSION['user']['firstName'];
+        }
+        return null;
+    }
+    
+    public function getFullName()
+    {
+        if (IsAuthenticated() == true) 
+        {
+            return $_SESSION['user']['firstName'] . " " . $_SESSION['user']['lastName'];
+        }
+        return null;
+    }
+    
+    public function getUserId()
+    {
+        if (IsAuthenticated() == true) 
+        {
+            return $_SESSION['user']['userId'];
+        }
+        return null;
+    }
 
 }
