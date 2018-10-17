fdfdf641aa3806759251e158bb0c5d2bbe99e44b
Geoffrey01 <longa.geoffrey@gmail.com>
2018-10-17 17:13:47 +0100

    no message

--- application/controllers/basket/BasketController.class.php
+++ application/controllers/basket/BasketController.class.php
@@ -3,7 +3,7 @@
 //verif de la connection on peut pas voir le panier si on est pas connecté, si le paner est vide créer quand même le panier mais vide
 class BasketController
 {
-  public function httpGetMethod(Http $http, array $formFields)
+  public function httpPostMethod(Http $http, array $formFields)
   {
     //Récupération donner formulaire du basket avec le bouton "valider la commande"
     $userSession = new UserSession();
@@ -26,7 +26,7 @@ class BasketController
 
   }
 
-  public function httpPostMethod(Http $http, array $formFields)
+  public function httpGetMethod(Http $http, array $formFields)
   {
     $userSession = new UserSession();
     if ($userSession->isAuthenticated() == false)
--- application/models/OrderModel.class.php
+++ application/models/OrderModel.class.php
@@ -5,15 +5,22 @@ class OrderModel
   public function findOrder($idOrder)
   {
     $database = new Database();
-    $sql = 'SELECT * FROM order WHERE Id = ?';
+    $sql =
+    'SELECT *
+    FROM order
+    WHERE Id = ?';
     return $database->queryOne($sql,[$idOrder]);
   }
 
   public function findOrderLine($idOrder)
   {
     $database = new Database();
-    $sql = 'SELECT QuantityOrder, Meal_Id, Name, PriceEach, Order_Id FROM orderline WHERE Order_Id = ?
-    INNER JOIN meal ON Meal.Id = Orderline.Meal_Id';
+    $sql =
+    'SELECT QuantityOrder, Meal_Id, Name, PriceEach, Order_Id
+    FROM orderline
+    INNER JOIN meal
+    ON Meal.Id = Orderline.Meal_Id
+    WHERE Order_Id = ?';
     return $database->query($sql,[$idOrder]);
   }
 
@@ -23,7 +30,7 @@ class OrderModel
     $database = new Database();
     $sqlOrder = $database->executeSql(
       'INSERT INTO order (User_Id, CreationTimeStamp, TaxRate)
-      VALUES (?, NOW(), 20), [$user])';
+      VALUES (?, NOW(), 20)', [$user]);
 
     $sqlOrderLine =
     'INSERT INTO orderline (Order_Id, Meal_Id, QuantityOrdered, PriceEach)
@@ -32,7 +39,7 @@ class OrderModel
     $TotalAmount = 0;
     // insertion des lignes de la commande
     foreach($basketItems as $basketItem){
-      $totalAmount += $basketItem['quantity'] * $basketItem['salePrice']
+      $totalAmount += $basketItem['quantity'] * $basketItem['salePrice'];
       // Ajout du montant HT de la ligne du panier au montant tota HT
       // Insertion d'une ligne de la commande dans la base de données
       $database->executeSql($sqlOrderLine, [$sqlOrder, $basketItem['Meal_Id'], $basketItem['QuantityOrdered'], $basketItem['SalePrice']]);
